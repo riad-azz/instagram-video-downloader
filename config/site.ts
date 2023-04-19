d4a9fcd15638f123c8d47fc51ae1dac067de04d2
Riadh <riadh.azzoun@hotmail.com>
2023-04-19 04:55:32 +0100

    Update Updated og image for a better resolution

--- config/site.ts
+++ config/site.ts
@@ -5,7 +5,7 @@ export const siteConfig: SiteConfig = {
   description:
     "Download Instagram videos and reels for free with our easy-to-use Instagram downloader tool. Save Instagram videos and reels in high quality with our fast, reliable downloader.",
   url: "https://riad-insta.vercel.app/",
-  ogImageUrl: "https://riad-insta.vercel.app/og.jpg",
+  ogImageUrl: "https://riad-insta.vercel.app/og.png",
   links: {
     twitter: "https://twitter.com/riadazz",
     github: "https://github.com/riad-azz/instagram-videos-downloader",
