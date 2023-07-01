c37f902e4b3073b790cda71223e0d0d6439340cf
Riadh <riad.azz.mail@gmail.com>
2023-07-01 11:54:03 +0100

    Update Added more SEO optimizations . Added google search console verification file to .gitignore .

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -36,43 +36,45 @@ const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
 
   return (
-    <nav
-      role="navigation"
-      className="mb-4 flex w-full flex-col items-center justify-between gap-2 border-b border-gray-300 px-2 py-1 shadow-md dark:border-gray-600 dark:shadow-black/25 md:flex-row md:px-8"
-    >
-      <div className="flex w-full items-center md:gap-2">
-        {/* Logo */}
-        <div className="mr-auto w-fit">
-          <LogoLink title="Insta Downloader" href="/" />
+    <header className="mb-4 w-full">
+      <nav
+        role="navigation"
+        className="flex w-full flex-col items-center justify-between gap-2 border-b border-gray-300 px-2 py-1 shadow-md dark:border-gray-600 dark:shadow-black/25 md:flex-row md:px-8"
+      >
+        <div className="flex w-full items-center md:gap-2">
+          {/* Logo */}
+          <div className="mr-auto w-fit">
+            <LogoLink title="Insta Downloader" href="/" />
+          </div>
+          {/* Dropdown menu button */}
+          <MenuButton onClick={() => setShowMenu(!showMenu)} />
+          {/* Main Navigation */}
+          <ul className="mr-2 hidden shrink-0 gap-2 md:flex">
+            {navbarLinks.map((link) => (
+              <NavLink key={link.id} href={link.href}>
+                {link.icon}
+                <span>{link.title}</span>
+              </NavLink>
+            ))}
+          </ul>
+          {/* Theme Toggle */}
+          <ThemeButton />
         </div>
-        {/* Dropdown menu button */}
-        <MenuButton onClick={() => setShowMenu(!showMenu)} />
-        {/* Main Navigation */}
-        <ul className="mr-2 hidden shrink-0 gap-2 md:flex">
+        {/* Mobile Menu Navigation */}
+        <ul
+          className={`mb-2 flex flex-col gap-2 max-md:w-full ${
+            !showMenu && "hidden"
+          } md:hidden`}
+        >
           {navbarLinks.map((link) => (
-            <NavLink key={link.id} href={link.href}>
+            <MobileMenuLink key={link.id} href={link.href}>
               {link.icon}
               <span>{link.title}</span>
-            </NavLink>
+            </MobileMenuLink>
           ))}
         </ul>
-        {/* Theme Toggle */}
-        <ThemeButton />
-      </div>
-      {/* Mobile Menu Navigation */}
-      <ul
-        className={`mb-2 flex flex-col gap-2 max-md:w-full ${
-          !showMenu && "hidden"
-        } md:hidden`}
-      >
-        {navbarLinks.map((link) => (
-          <MobileMenuLink key={link.id} href={link.href}>
-            {link.icon}
-            <span>{link.title}</span>
-          </MobileMenuLink>
-        ))}
-      </ul>
-    </nav>
+      </nav>
+    </header>
   );
 };
 
--- src/configs/site.ts
+++ src/configs/site.ts
@@ -1,9 +1,9 @@
 import { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
-  name: "Free Instagram Downloader",
+  name: "Insta Videos Downloader",
   description:
-    "Discover our powerful Instagram content downloader, an exceptional tool for downloading high-quality Instagram media at absolutely no cost. Experience the ease of saving videos and reels with just a few simple steps, all without the hassle of logging in.",
+    "Download Instagram videos instantly with Insta Videos Downloader. Save and enjoy your favorite content offline with our easy-to-use tool.",
   url: "https://riad-insta.vercel.app/",
   ogImageUrl: "https://riad-insta.vercel.app/og.png",
   links: {
