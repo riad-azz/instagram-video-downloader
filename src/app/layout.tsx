aebb898ea9477d36762a43b975f4a49784c9fe38
Riadh <riadh.azzoun@hotmail.com>
2024-11-02 06:26:53 +0100

    fix: bad footer & navbar root layout imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,14 @@
 import { Metadata } from "next";
 import { DM_Sans as FontSans } from "next/font/google";
 
-import "@/styles/globals.css";
-
-import { Navbar } from "@/components/navbar";
-import { Footer } from "@/components/footer";
+import { Navbar, Footer } from "@/components/layout";
 
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ReactQueryProvider } from "@/components/providers/react-query-provider";
 
 import { cn } from "@/lib/utils";
+
+import "./globals.css";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -31,7 +30,7 @@ export default function RootLayout({
       <body
         className={cn(
           fontSans.variable,
-          "bg-background overflow-x-hidden font-sans antialiased"
+          "overflow-x-hidden bg-background font-sans antialiased"
         )}
       >
         <ThemeProvider
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,13 +1,12 @@
 "use client";
 
 import React from "react";
-
 import Link from "next/link";
-import Image from "next/image";
+
+import { MobileNav } from "./mobile-nav";
+import { ThemeToggle } from "./theme-toggle";
 
 import { cn } from "@/lib/utils";
-import { ThemeToggle } from "./theme-toggle";
-import { MobileNav } from "./mobile-nav";
 
 export function Navbar() {
   return (
@@ -16,7 +15,7 @@ export function Navbar() {
         className={cn(
           "z-50",
           "flex h-[3.5rem] px-4",
-          "bg-accent/20 w-full items-center border-b"
+          "w-full items-center border-b bg-accent/20"
         )}
       >
         <div className="hidden select-none text-xl font-extrabold uppercase md:block">
@@ -47,5 +46,28 @@ export function Navbar() {
         </div>
       </nav>
     </header>
+  );
+}
+
+export function Footer() {
+  return (
+    <footer className="h-8 w-full bg-background/50 text-center text-secondary-foreground">
+      <div className="flex items-center justify-between border-t border-input px-4 py-2">
+        <div aria-label="Disclaimer" className="text-sm font-semibold">
+          We are not affiliated with Instagram or Meta
+        </div>
+        <div className="flex items-center gap-1 text-sm">
+          <Link href="/terms-of-service" className="hover:underline">
+            Terms of Service
+          </Link>
+          <span aria-hidden="true" className="mx-2 select-none">
+            |
+          </span>
+          <Link href="/privacy-policy" className="hover:underline">
+            Privacy Policy
+          </Link>
+        </div>
+      </div>
+    </footer>
   );
 }
